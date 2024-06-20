2d89e2a3709a9a6c0e42cb13731410a0b7b3bb70
yakter yeuda <143093738+YeudaYakter@users.noreply.github.com>
2024-06-20 10:09:13 +0300

    fix conflicts

--- backend/routs/postNetwoek.js
+++ backend/routs/postNetwoek.js
@@ -1,21 +1,10 @@
 const express = require("express");
-<<<<<<< HEAD
-
-const router = express.Router();
-const {PostModel} = require("../models/posts.js")
-
-router.get("/", async (req, res) => {
-=======
 
 const router = express.Router();
 
 const {PostModel} = require("../models/posts")
 
 router.get("/", async (req, res) => {
-    let data = await PostModel.find({});
-    res.json(data)
->>>>>>> 946f4b172dfa0f22c4993a3e17f585efe220424c
-    
     let data = await PostModel.find({});
     res.json(data)
 

b116b4eb4a16fcadee619f3dd70baeb41333919c
priel gozlan <prielgozlan@gmail.com>
2024-06-20 10:04:57 +0300

    fix some changes

--- backend/routs/confet_raauts.js
+++ backend/routs/confet_raauts.js
@@ -3,9 +3,5 @@ const postsR = require("./postNetwoek");
 
 exports.routesInit = (app) => {
   app.use("/posts", postsR);
-<<<<<<< HEAD
-  app.use("/users/", socialR);
-=======
   app.use("/users", socialR);
->>>>>>> 946f4b172dfa0f22c4993a3e17f585efe220424c
 };
--- backend/routs/socialNetwork.js
+++ backend/routs/socialNetwork.js
@@ -46,7 +46,7 @@ router.post("/",async (req,res) => {
 
 router.post("/login" , async(req,res) => {
   
-    let validBody =  validLogin (req.body);
+    let validBody =  validLogin(req.body);
 
     if (validBody.error) {
       return res.status(400).json(validBody.error.details);

9b72dc145cf57dce984566fa0986798fb2354f6a
yakter yeuda <143093738+YeudaYakter@users.noreply.github.com>
2024-06-24 14:31:41 +0300

    update

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,42 +1,40 @@
 const mongoose = require("mongoose");
 
 const Joi = require("joi");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const userSchema = new mongoose.Schema({
-    name:String,
-    avatar:String,
-    createdAt:{type:Date, default:Date.now() },
-    username:String,
-    friends:Array,
-    my_posts:Array,
-    email:String,
-    pass:{type:String, select:false},
- 
-  });
-  
-  exports.UserModel = mongoose.model("users",userSchema);
-  
-  exports.gettoken=(user)=>{
+  name: String,
+  createdAt: { type: Date, default: Date.now() },
+  friends: Array,
+  email: String,
+  pass: { type: String, select: false },
 
-    let token = jwt.sign({user},"yeudayakter",{expiresIn:"60day"});
-    return token
-  }
- 
-  exports.validUser = (_bodyData) => {
-    let joiSchema = Joi.object({
-      name:Joi.string().min(2).max(99).required(),
-      email:Joi.string().min(2).max(300).required().email(),
-      pass:Joi.string().min(3).max(100).required(),
-    })
-  
-    return joiSchema.validate(_bodyData);
-  }
-  exports.validLogin = (_bodyData) => {
-    let joiSchema = Joi.object({
-      name:Joi.string().min(2).max(300).required(),
-      pass:Joi.string().min(3).max(100).required(),
-    })
-  
-    return joiSchema.validate(_bodyData);
-  }
-  
+  address: String,
+
+  Marital_Status: String,
+});
+
+exports.UserModel = mongoose.model("users", userSchema);
+
+exports.gettoken = (user) => {
+  let token = jwt.sign({ user }, "yeudayakter", { expiresIn: "60day" });
+  return token;
+};
+
+exports.validUser = (_bodyData) => {
+  let joiSchema = Joi.object({
+    name: Joi.string().min(2).max(99).required(),
+    email: Joi.string().min(2).max(300).required().email(),
+    pass: Joi.string().min(3).max(100).required(),
+  });
+
+  return joiSchema.validate(_bodyData);
+};
+exports.validLogin = (_bodyData) => {
+  let joiSchema = Joi.object({
+    name: Joi.string().min(2).max(300).required(),
+    pass: Joi.string().min(3).max(100).required(),
+  });
+
+  return joiSchema.validate(_bodyData);
+};
--- backend/routs/postNetwoek.js
+++ backend/routs/postNetwoek.js
@@ -2,12 +2,39 @@ const express = require("express");
 
 const router = express.Router();
 
-const {PostModel} = require("../models/posts")
+const {validPost, PostModel} = require("../models/posts")
+const { authToken } = require("../auth/authToken.js");
 
 router.get("/", async (req, res) => {
     let data = await PostModel.find({});
     res.json(data)
 
 })
+
+router.post("/" , authToken, async(req,res) => {
+    // let valdiateHeader = validPost(req.header);
+    // let valdiateBody = validPost(req.body);
+    let valdiateBody = validPost(req.body);
+    
+
+    
+    console.log(valdiateBody);
+    if(valdiateBody.error){
+      return res.status(400).json(valdiateBody.error.details)
+    }
+    try{
+      let post = new PostModel(req.body);
+
+      post.user_id = req.tokenData.user._id;
+      console.log(post.user_id)
+      await post.save();
+      res.status(201).json({post});
+    }
+    catch(err){
+      console.log(err);
+      res.status(500).json(err)
+    }
+  })
+
 
 module.exports = router

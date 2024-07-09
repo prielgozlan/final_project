b00cf444f7189f633af9af345ecd59d54800a168
priel gozlan <prielgozlan@gmail.com>
2024-07-09 12:17:37 +0300

    add callback to DB connection

--- backend/db/mongoConect.js
+++ backend/db/mongoConect.js
@@ -5,7 +5,9 @@ main().catch(err => console.log(err));
 
 async function main() {
   // await mongoose.connect('mongodb://127.0.0.1:27017/Social_Network_nies');
-  await mongoose.connect(process.env.DB_URL);
+  await mongoose.connect(process.env.DB_URL)
+    .then(() => console.log("Database connected!"))
+    .catch(err => console.log(err));;
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   // console.log("mongo exssiting");
